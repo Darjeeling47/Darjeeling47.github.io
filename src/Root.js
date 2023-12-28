90142be8ca6fd8e80445ce480fe3914a3932511d
Piinxluna <pingpingkuss@gmail.com>
2023-12-28 13:22:49 +0700

    edit navbar

--- src/Root.js
+++ src/Root.js
@@ -1,28 +1,27 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet, useLocation } from "react-router-dom";
 
 //css import
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 //json
-import Data from './Root.json'
+import Data from "./Root.json";
 
 function Root() {
-  return(
+  return (
     <>
       <NavigationBar />
       <Outlet />
-      <div className='mb-5'></div>
+      <div className="mb-5"></div>
     </>
   );
 }
 
 function NavigationBar() {
-
   let location = useLocation().pathname;
 
-  return(
+  return (
     <Navbar expand="lg" className="color-bg-neutral-100">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -38,12 +37,14 @@ function NavigationBar() {
 
 function NavigationMenu() {
   return Data.map((page) => {
-    return(
+    return (
       <Nav.Link
-        href={'/' + page.path}
+        href={"/profile/" + page.path}
         key={page.path}
-        className="navigation-bar-menu me-1">
-        <i className={page.icon}></i>{page.title}
+        className="navigation-bar-menu me-1"
+      >
+        <i className={page.icon}></i>
+        {page.title}
       </Nav.Link>
     );
   });
